55adeecb18e7cb7e89e1c3c9d80532a1235255e5
steviesocks <stephen.stocking@gmail.com>
2020-06-24 10:48:38 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const knex = require('knex');
+const cors = require('cors');
+
+const db = knex({
+    client: 'pg',
+        connection: {
+        host : '127.0.0.1',
+        user : 'stephenstocking',
+        password : '',
+        database : 'book-library'
+    }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res)=> {
+    res.send('it is working!');
+    console.log("get request")
+})
+
+app.get('/browse', (req, res)=> {
+    db.select('*').from('books')
+        .then(response => {
+            response.length
+                ? res.json(response)
+                : res.status(400).json('whoopsie daisy! something went wrong...')
+        })
+        .catch(err => console.log('we caught an error'))
+})
+
+
+app.post('/player', (req, res) => {
+    const { id } = req.body;
+    db.select('*').from('books')
+    .where({
+        id: id
+    })
+    .then(result => {
+        result.length 
+        ? res.json(result)
+        : res.status(400).json('book not found')
+    })
+    .catch(err => res.status(400).json('bad request'))
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+    })
