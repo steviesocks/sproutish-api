466efd540506aa6cdf460142a3d7e5370d362abf
steviesocks <stephen.stocking@gmail.com>
2020-06-30 11:09:33 -0400

    undoing previous change

--- server.js
+++ server.js
@@ -17,13 +17,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
-  });
-
 app.get('/', (req, res)=> {
     res.send('it is working!');
 })
