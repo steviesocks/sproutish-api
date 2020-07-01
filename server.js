23c1a9e3599d3c388da56d37c075b0f4343853eb
steviesocks <stephen.stocking@gmail.com>
2020-07-01 11:14:52 -0400

    pruning commented code

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/*', (req, res) => {
-//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//  });
 
 app.get('/', (req, res)=> {
     res.send('it is working!');
