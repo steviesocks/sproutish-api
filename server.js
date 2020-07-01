ba9ab8cdf6c376fa39482670db3242ed4c04ab09
steviesocks <stephen.stocking@gmail.com>
2020-06-30 20:42:03 -0400

    moving to HashRouter

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-+app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+// app.get('/*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
 
-// app.get('/', (req, res)=> {
-//     res.send('it is working!');
-// })
+app.get('/', (req, res)=> {
+    res.send('it is working!');
+})
 
 app.get('/browse', (req, res)=> {
     db.select('*').from('books')
